2e77590ac479f8ae46cb9d8beeb10db5e172d486
Jhonas <profilejass@gmail.com>
2025-08-30 19:48:01 -0500

    feat: Implement Product API with CRUD operations and category management

---
+++ src/products/domain/model/product.model.ts
@@ -0,0 +1,10 @@
+export class Product {
+    id!: number;
+    name!: string;
+    price!: number;
+    quantity!: number;
+    status!: string;
+    categoryId!: number;
+}
+
+export default Product;
